18688e956142d3350c6963162dc23b38d862d9c8
fengxuan.liu <fengxuan.liu@nttdata.com>
2019-06-10 16:12:09 +0800

    test

--- src/main/java/com/lucifer/Application.java
+++ src/main/java/com/lucifer/Application.java
@@ -4,13 +4,14 @@ package com.lucifer;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 /**
  * Created by lijc on 15/8/27.
  */
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
