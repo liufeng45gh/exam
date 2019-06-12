6f7710e657d1033713d869207684e07f7c4aef0f
fengxuan.liu <fengxuan.liu@nttdata.com>
2019-06-12 10:03:41 +0800

    test

--- src/main/java/com/lucifer/utils/RequestUtils.java
+++ src/main/java/com/lucifer/utils/RequestUtils.java
@@ -7,6 +7,9 @@ public class RequestUtils {
 
     public static String getCookie(String name, HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
+        if(null ==cookies){
+            return null;
+        }
         for (Cookie cookie : cookies) {
             if (cookie.getName().equals(name)) {
                 return cookie.getValue();
